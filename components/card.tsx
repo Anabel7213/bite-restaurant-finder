3130d8a287856d6f5bf46df7d8a0419cbc1807ff
Anastasia Bielievitina <abielievitina@gmail.com>
2024-03-10 19:39:24 -0400

    latest

--- components/card.tsx
+++ components/card.tsx
@@ -23,7 +23,7 @@ export default function RestaurantCard({item, favorites, setFavorites, userLocat
             <div key={item?.place_id} className="flex shrink-0 flex-col border border-black rounded-[4px] bg-white p-6 w-full md:w-[236px]">
             <Image
             src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${
-                item?.photos?.[0]?.photo_reference ? item.photos[0].photo_reference : 'defaultPhotoReference'
+                item?.photos?.[0]?.photo_reference ? item?.photos[0]?.photo_reference : 'defaultPhotoReference'
             }&key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}`}
             alt={item?.name}
             width={100}
@@ -33,13 +33,13 @@ export default function RestaurantCard({item, favorites, setFavorites, userLocat
                 <div className="flex justify-between items-center">
                     <div className="flex items-center gap-1">
                         {item?.rating}
-                        <Rating value={item.rating} size="small" readOnly icon={ <Image src="star.svg" alt="Rating" width={16} height={16} />} emptyIcon={<Image src="emptyStar.svg" alt="Rating" width={16} height={16} />} />
-                        <span>{item?.price_level === 1 ? "·$" : item.price_level === 2 ? "·$$" : item.price_level === 3 ? "·$$$" : item.price_level === 4 ? "·$$$$" : "·$$"}</span>
+                        <Rating value={item?.rating} size="small" readOnly icon={ <Image src="star.svg" alt="Rating" width={16} height={16} />} emptyIcon={<Image src="emptyStar.svg" alt="Rating" width={16} height={16} />} />
+                        <span>{item?.price_level === 1 ? "·$" : item?.price_level === 2 ? "·$$" : item?.price_level === 3 ? "·$$$" : item?.price_level === 4 ? "·$$$$" : "·$$"}</span>
                     </div>
-                    <span className={item?.opening_hours?.open_now === true ? "bg-[#BBCC3F] text-sm px-[4px] border-black border rounded-[4px]" : "bg-[#FFADC0] text-sm px-[4px] border-black border rounded-[4px]"}>{item?.opening_hours.open_now === true ? "Open" : "Closed"}</span>
+                    <span className={item?.opening_hours?.open_now === true ? "bg-[#BBCC3F] text-sm px-[4px] border-black border rounded-[4px]" : "bg-[#FFADC0] text-sm px-[4px] border-black border rounded-[4px]"}>{item?.opening_hours?.open_now === true ? "Open" : "Closed"}</span>
                 </div>
-                <h1 className="capitalize font-medium text-lg">{item?.name.length > 18 ? item.name.slice(0,20) + "...": item.name}</h1>
-                <p>{item?.formatted_address.substring(0, item?.formatted_address.lastIndexOf(",")).slice(0, 43)}</p>
+                <h1 className="capitalize font-medium text-lg">{item?.name?.length > 18 ? item?.name.slice(0,20) + "...": item?.name}</h1>
+                <p>{item?.formatted_address.substring(0, item?.formatted_address?.lastIndexOf(",")).slice(0, 43)}</p>
                 <div className="flex items-center gap-2">
                     <div className="p-2 rounded-[4px] bg-slate-100 border border-black"><Heart onClick={handleSubmit} fill={fav ? "#FF8195" : "#FFFFFF"} strokeWidth={1.5} size={20} className="top-[10%] left-[18.5%] cursor-pointer" /></div>
                     <button onClick={getDirections} className="p-2 rounded-[4px] border-black border w-full">Directions</button>
--- components/markers.tsx
+++ components/markers.tsx
@@ -11,10 +11,10 @@ const Markers = ({ place, userLocation }) => {
 
   return (
     <MarkerF
-      key={place.place_id}
-      position={place.geometry.location}
+      key={place?.place_id}
+      position={place?.geometry.location}
       icon={
-        place.opening_hours?.open_now
+        place?.opening_hours?.open_now
           ? { url: "markerOpen.svg", scaledSize: { equals: () => false, width: 24, height: 24 } }
           : { url: "markerClosed.svg", scaledSize: { equals: () => false, width: 24, height: 24 } }
       }
@@ -22,25 +22,25 @@ const Markers = ({ place, userLocation }) => {
     >
       {selectedPlace?.reference === place.reference ? (
         <OverlayView position={selectedPlace.geometry.location} mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}>
-          <div key={place.place_id} className="flex shrink-0 flex-col border border-black rounded-[4px] bg-white p-4 w-fit">
+          <div key={place?.place_id} className="flex shrink-0 flex-col border border-black rounded-[4px] bg-white p-4 w-fit">
             <div className="flex justify-between items-center gap-2">
               <div className="flex items-center gap-1">
-                {place.rating}
+                {place?.rating}
                 <Rating
-                  value={place.rating}
+                  value={place?.rating}
                   size="small"
                   readOnly
                   icon={<Image src="star.svg" alt="Rating" width={12} height={12} />}
                   emptyIcon={<Image src="emptyStar.svg" alt="Rating" width={12} height={12} />}
                 />
-                <span>{place.price_level === 1 ? "·$" : place.price_level === 2 ? "·$$" : place.price_level === 3 ? "·$$$" : place.price_level === 4 ? "·$$$$" : "·$$"}</span>
+                <span>{place?.price_level === 1 ? "·$" : place?.price_level === 2 ? "·$$" : place?.price_level === 3 ? "·$$$" : place?.price_level === 4 ? "·$$$$" : "·$$"}</span>
               </div>
               <button onClick={getDirections} className="p-2 hover:bg-slate-100 rounded-[4px] transition-all">
                 <MapIcon size={20} strokeWidth={1.5} />
               </button>
             </div>
             <h1 className="capitalize font-medium text-sm">{place.name}</h1>
-            <p>{place.formatted_address.substring(0, place.formatted_address.lastIndexOf(",")).slice(0, 43)}</p>
+            <p>{place?.formatted_address.substring(0, place?.formatted_address.lastIndexOf(",")).slice(0, 43)}</p>
           </div>
         </OverlayView>
       ) : null}
